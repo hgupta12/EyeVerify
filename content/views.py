5d02e7631b489ba2d47237a02c2fa6652778baea
harshit <harshitgupta1406@gmail.com>
2023-09-06 21:45:39 +0530

    Make Content Immutable

--- content/views.py
+++ content/views.py
@@ -2,9 +2,17 @@ from rest_framework import viewsets
 from content.models import Content
 from .serializers import ContentSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.exceptions import MethodNotAllowed
 
 # Create your views here.
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
     parser_classes = (MultiPartParser,FormParser)
+
+    def update(self,request,*args, **kwargs):
+        raise MethodNotAllowed("PUT is not allowed for this resource")
+    
+    def partial_update(self, request, *args, **kwargs):
+        raise MethodNotAllowed("PATCH is not allowed for this resource")
+        
